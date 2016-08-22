32eadd5f6cc945c361aa94179aa249c3d2adb8dd
Jan Klimo <jan.klimo@gmail.com>
2016-08-22 18:00:20 +0700

    emoji test

--- tweet-client.rb
+++ tweet-client.rb
@@ -10,12 +10,12 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
 end
 
-image_kit = IMGKit.new('http://localhost:3000/charts/2016-08-20?rank=2',
+image_kit = IMGKit.new('https://ph-tweet-server.herokuapp.com/charts/2016-08-21?rank=2',
                        zoom: 2, width: 2048, height: 1024)
 
 puts "Saving the preview..."
-img = image_kit.to_file('charts/attachment.jpg')
+img = image_kit.to_file('attachment.jpg')
 
-client.update_with_media('Image upload test', img)
+client.update_with_media('👍👏', img)
 
 puts "Done!"
